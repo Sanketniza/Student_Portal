d461c6334c560ce0a1b0baa8ae1b798c00ad8966
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-08-19 02:17:52 +0530

    feat: Implemented job filtering and search functionality in the browser component

--- Frontend/src/components/Browser.jsx
+++ Frontend/src/components/Browser.jsx
@@ -1,13 +1,17 @@
 import Navbar from "./shared/Navbar"
 
+//&------------------------------------------------------------------------------------------
 
+const randomJobs = [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+//&------------------------------------------------------------------------------------------
 function Browser() {
   return (  
         <>
             <Navbar />
 
-            <div className="">
-
+            <div className="mx-auto my-10 max-w-7xl sm:px-6 lg:px-8 ">
+                <h1 className="text-lg font-bold text-zinc-800">Search Results ({randomJobs.length})</h1>
             </div>
         </>
     )
--- Frontend/src/components/FilterCard.jsx
+++ Frontend/src/components/FilterCard.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { Label } from "@radix-ui/react-label";
+import { RadioGroup, RadioGroupItem } from './ui/radio-group'
+
+const filterData = [
+    {
+        filterType: "Location",
+        array: ["Delhi", "Sanket", "Faridabad", "Greater", "Greater Nagpur", "Greater Pune", "Greater Mumbai"]
+    },
+    {
+        filterType: "Role",
+        array: ["Frontend", "Backend", "Fullstack"]
+    },
+    {
+        filterType: "Experience",
+        array: ["0-2 years", "2-5 years", "5-10 years", "10+ years"]
+    },
+    {
+        filterType: "Salary",
+        array: ["0 - 20k", "20 - 50k", "50 - 100k", "100 - 200k", "200 - 300k", "300 - 400k", "400 - 500k"]
+    }
+];
 
 function FilterCard() {
-  return (
-    <div>FilterCard</div>
-  )
+    return (
+        <>
+            <div className="w-full p-4 overflow-y-auto border border-gray-300 rounded-lg shadow shadow-2xl h-[80vh]">
+                <h1 className="text-xl font-bold shadow-2xl text-zinc-800 ">Filter Jobs</h1>
+                <hr className="h-1 my-2 bg-gray-300" />
+                
+                <div>
+                <RadioGroup >
+                {
+                    filterData.map((data, index) => (
+                        <div key={index}>
+                            <h1 className='font-medium font-bold'>{data.filterType}</h1>
+                            {
+                                data.array.map((item, idx) => {
+                                    const itemId = `id${index}-${idx}`
+                                    return (
+                                        <div className='flex items-center my-2 space-x-2' key={idx}>
+                                            <RadioGroupItem value={item} id={itemId} />
+                                            <Label htmlFor={itemId}>{item}</Label>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    ))
+                }
+            </RadioGroup> 
+                </div>
+                
+            </div>
+        </>
+    );
 }
 
-export default FilterCard
+export default FilterCard;
