ee426e338ddc84edade01cfd409003b0eece6744
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-08-19 02:29:47 +0530

    feat: Added Job component and Jobs import to Browser component for job display

--- Frontend/src/components/Browser.jsx
+++ Frontend/src/components/Browser.jsx
@@ -1,8 +1,10 @@
+import Job from "./Job";
+import Jobs from "./Jobs";
 import Navbar from "./shared/Navbar"
 
 //&------------------------------------------------------------------------------------------
 
-const randomJobs = [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+const randomJobs = [ 1,2,3,4,5,6,7,8,9,10,11,12];
 
 //&------------------------------------------------------------------------------------------
 function Browser() {
@@ -11,7 +13,18 @@ function Browser() {
             <Navbar />
 
             <div className="mx-auto my-10 max-w-7xl sm:px-6 lg:px-8 ">
-                <h1 className="text-lg font-bold text-zinc-800">Search Results ({randomJobs.length})</h1>
+                <h1 className="text-lg font-bold text-zinc-800">Search Results ({randomJobs.length })</h1>
+                
+                <div className="grid grid-cols-1 gap-4 my-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">      
+                    {
+                        randomJobs.map((item) => {
+                            return (
+                                <Job/>
+                            )
+                        })
+                    }
+                </div>
+                
             </div>
         </>
     )
