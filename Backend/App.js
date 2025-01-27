9e86c4e16e0eafaad2abb052cd0aad1c1547624a
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-01-28 01:06:04 +0530

    updates

--- Backend/App.js
+++ Backend/App.js
@@ -1,13 +1,17 @@
-
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";          // for cross-origin resource sharing . naturally used to connect backend and frontend
-import dotenv from "dotenv";      // for environment variables , to create and use .env file for security 
+import cors from "cors"; // for cross-origin resource sharing . naturally used to connect backend and frontend
+import dotenv from "dotenv"; // for environment variables , to create and use .env file for security 
 import connectDB from "./util/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRouter from "./routes/job.route.js";
 import application from "./routes/application.route.js";
+
+import authRoutes from './routes/authRoutes.js';
+import { ClerkExpressRequireAuth, ClerkExpressWithAuth, Clerk } from '@clerk/clerk-sdk-node';
+
+Clerk({ apiKey: 'pk_test_ZnVuLXdvbGYtMC5jbGVyay5hY2NvdW50cy5kZXYk' });
 
 
 //? ------------------------------------------------------------------------------------------------------
@@ -33,9 +37,9 @@ app.use(express.json()); // All the data in the form of JSON, Parse JSON request
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 app.use(cookieParser());
 const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true,            //access-control-allow-credentials:true
-  // optionSuccessStatus: 200
+    origin: 'http://localhost:5173',
+    credentials: true, //access-control-allow-credentials:true
+    // optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
 
@@ -43,7 +47,7 @@ app.use(cors(corsOptions));
 //? ------------------------------------------------------------------------------------------------------
 
 //^ api creating 
-
+app.use('/api/auth', authRoutes);
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/company', companyRoute);
 app.use('/api/v1/job', jobRouter);
@@ -54,17 +58,17 @@ app.use('/api/v1/application', application);
 //^ Define routes....
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 
 //? ------------------------------------------------------------------------------------------------------
 
 //^ Start the server
-const port =  process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  connectDB();
-  console.log(`Server is running on port ${port}`);
+    connectDB();
+    console.log(`Server is running on port ${port}`);
 });
 
 
